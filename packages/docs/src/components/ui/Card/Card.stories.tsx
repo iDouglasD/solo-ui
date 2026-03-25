57b618066b9ce5bfcdbf87cd48aa2c421c782580
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-03-25 12:28:05 -0300

    fix(docs): fix import order in Card stories

--- packages/docs/src/components/ui/Card/Card.stories.tsx
+++ packages/docs/src/components/ui/Card/Card.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react'
 import { Card } from '@solo-ui/ui'
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof Card> = {
   title: 'UI/Card',
