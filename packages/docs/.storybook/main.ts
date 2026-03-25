6dc5a0a48e788adef7c9fca16d8fe996e928827f
Douglas Duarte <douglasduarte.eng@gmail.com>
2026-03-25 09:06:56 -0300

    chore(docs): add Tailwind v4 setup to Storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,8 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../../react/src/**/*.stories.@(ts|tsx)'],
   framework: '@storybook/react-vite',
+  viteFinal: async (config) => {
+    config.plugins = [...(config.plugins ?? []), tailwindcss()]
+    return config
+  },
 }
 
 export default config
